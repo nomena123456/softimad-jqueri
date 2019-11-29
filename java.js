6733966948ec6984f7a4b044282c410148036280
nomenarasolonjatovo <nomenarasolo98@gmail.com>
2019-11-29 16:16:56 +0300

    safwe

--- java.js
+++ java.js
@@ -9,6 +9,19 @@ $(document).ready(function(){
              slidesToScroll: 2,
              nextArrow:false,
              prevArrow:false,
+             responsive: [
+                {
+                  breakpoint: 720,
+                  settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+
+                  }
+                }
+                // You can unslick at a given breakpoint now by adding:
+                // settings: "unslick"
+                // instead of a settings object
+              ]
 
 
         
@@ -21,6 +34,29 @@ $(document).ready(function(){
             infinite: true,
             slidesToShow: 4,
              slidesToScroll: 1,
+             responsive: [
+                {
+                  breakpoint: 1000,
+                  settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+
+                  }
+
+                },
+                {
+                  breakpoint: 720,
+                  settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+
+                  }
+                  
+                }
+                // You can unslick at a given breakpoint now by adding:
+                // settings: "unslick"
+                // instead of a settings object
+              ]
 
         
         });
@@ -33,6 +69,28 @@ $(document).ready(function(){
             infinite: true,
             slidesToShow: 4,
              slidesToScroll: 1,
+             responsive: [
+                {
+                  breakpoint: 1000,
+                  settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+
+                  }
+                },
+                {
+                  breakpoint: 720,
+                  settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+
+                  }
+                  
+                }
+                // You can unslick at a given breakpoint now by adding:
+                // settings: "unslick"
+                // instead of a settings object
+              ]
 
         
         });
