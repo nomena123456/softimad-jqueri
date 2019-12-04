5051e4327a25191406fb83830db434e712aec979
nomenarasolonjatovo <nomenarasolo98@gmail.com>
2019-12-04 09:38:40 +0300

    wwerwe

--- java.js
+++ java.js
@@ -5,25 +5,10 @@ $(document).ready(function(){
             speed: 1000,
             dots: true,
             infinite: true,
-            slidesToShow: 2,
-             slidesToScroll: 2,
+            slidesToShow: 1,
+             slidesToScroll: 1,
              nextArrow:false,
              prevArrow:false,
-             responsive: [
-                {
-                  breakpoint: 720,
-                  settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-
-                  }
-                }
-                // You can unslick at a given breakpoint now by adding:
-                // settings: "unslick"
-                // instead of a settings object
-              ]
-
-
         
         });
         $('#img-contenu').slick({
